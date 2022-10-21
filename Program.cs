d612e958e2064c1af72a95a76274b39685495021
JLamzon <jlamzon@yahoo.com>
2022-10-21 10:41:55 -0700

    added peer review to program

--- Program.cs
+++ Program.cs
@@ -2,6 +2,16 @@
 //10/20/22
 //GitHubChallenge - Redo mini Challenges
 // Guess It - data validation and play again
+//Peer Review by Amardeep Mann: Good work with 
+// the play-again feature. Using the while loop was a great call for that. 
+// Choosing the game mode works well enough. For future evesions you may want 
+// to throw in a data validation to make sure the user picks a valid difficulty 
+// instead of going straight to "Play Again?" if they don't. The custom game mode 
+// also works pretty great. The only way I was able to break it is if I choose a 
+// aximum number that's lower than the min. You may want to add a check in the future 
+// to account for users being idiots since it will crash the program. All in all great 
+// work! I appreciates how you ask the user for their name to make for a more interactive 
+// experience
 
 
 Console.Clear();
